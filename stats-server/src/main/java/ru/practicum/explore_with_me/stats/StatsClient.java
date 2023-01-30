4a2f1db036f0364a3357474a0be49cc0adac0acb
sabolenok <d5sp2004@yandex.ru>
2023-01-30 16:23:08 +0530

    Исправление замечаний по CR: изменения в HTTP-client для сервиса статистики

--- stats-server/src/main/java/ru/practicum/explore_with_me/stats/StatsClient.java
+++ stats-server/src/main/java/ru/practicum/explore_with_me/stats/StatsClient.java
@@ -1,32 +1,56 @@
 package ru.practicum.explore_with_me.stats;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.explore_with_me.stats.client.BaseClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import ru.practicum.explore_with_me.stats.dto.StatWithCount;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
 
 @Service
-public class StatsClient extends BaseClient {
+@RequiredArgsConstructor
+public class StatsClient {
 
-    @Autowired
-    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
-        super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                        .build()
-        );
+    @Value("${stats-server.url}")
+    private String SERVER_URL;
+    private final RestTemplate rest = new RestTemplate();
+
+    public void hit(Stat stat) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Stat> requestEntity = new HttpEntity<>(stat, headers);
+
+        rest.exchange(SERVER_URL + "/hit", HttpMethod.POST, requestEntity, Stat.class);
     }
 
-    public ResponseEntity<Object> addStat(Stat stat) {
-        return post("", stat);
-    }
+    public List<StatWithCount> getStats(String start, String end, List<String> uris, Boolean unique)
+            throws JsonProcessingException {
+        Map<String, Object> parameters = new HashMap<>();
 
-    public ResponseEntity<Object> getStats() {
-        return get("");
+        parameters.put("start", start);
+        parameters.put("end", end);
+        parameters.put("uris", uris);
+        parameters.put("unique", unique);
+
+        ResponseEntity<String> response = rest.getForEntity(
+                SERVER_URL + "/stats?start={start}&end={end}&uris={uris}&unique={unique}",
+                String.class, parameters);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        StatWithCount[] array = objectMapper.readValue(response.getBody(), StatWithCount[].class);
+
+        return Arrays.asList(array);
     }
 }
