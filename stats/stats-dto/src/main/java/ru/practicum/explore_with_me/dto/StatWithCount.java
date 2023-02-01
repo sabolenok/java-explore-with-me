62eb3cc6fe898bd88706efe5f7c344727ab5f32d
sabolenok <d5sp2004@yandex.ru>
2023-02-01 18:06:25 +0530

    Проверка на валидность дат, пришедших в запросе. Рефакторинг метода getStats

--- stats/stats-dto/src/main/java/ru/practicum/explore_with_me/dto/StatWithCount.java
+++ stats/stats-dto/src/main/java/ru/practicum/explore_with_me/dto/StatWithCount.java
@@ -1,8 +1,10 @@
 package ru.practicum.explore_with_me.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StatWithCount {
     private String app;
     private String uri;
